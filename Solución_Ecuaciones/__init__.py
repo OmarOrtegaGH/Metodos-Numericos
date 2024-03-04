28d325bb39189f764655c70e86450b11b64ed56e
Omar Emiliano Ortega Torres <161262326+OmarOrtegaGH@users.noreply.github.com>
2024-03-04 00:05:39 -0600

    Add files via upload

---
+++ Solución_Ecuaciones/__init__.py
@@ -0,0 +1,10 @@
+#Este init es una prueba, no está terminado
+#Solución_Ecuaciones/__init__.py
+
+#from .root.#bisección import Bis, roo_bis
+#from .root.#Newton import NR, roo_NR
+#from .root.#Punto_fijo import PF, roo_PF
+#from .root.#secv1 import secv1, roo_secv1
+#from .root.#secv2 import secv2, roo_secv2
+
+#__all__ = ['Bis','roo_Bis',etc etc]
