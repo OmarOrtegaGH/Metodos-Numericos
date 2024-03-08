86160bf7ff2e6aefe59d562dabed0fc1836867c8
Omar Emiliano Ortega Torres <161262326+OmarOrtegaGH@users.noreply.github.com>
2024-03-08 13:42:37 -0600

    Update __init__.py

--- Solución_Ecuaciones/root/__init__.py
+++ Solución_Ecuaciones/root/__init__.py
@@ -3,5 +3,7 @@
 from .BisecciónV3 import biseccion
 from .Newton_RaphsonV3 import newton
 from .Punto_FijoV3 import puntofijo
+from .SecV1 import secv1
+from .SecV2 import secv2
 
-__all__ = ['biseccion', 'newton', 'puntofijo']
+__all__ = ['biseccion', 'newton', 'puntofijo', 'secv1', 'secv2']
